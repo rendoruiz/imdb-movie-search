a73f0e0ad2854962e644fdbbbc471177402b39b0
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-27 21:34:38 -0600

    search box

--- src/App.js
+++ src/App.js
@@ -1,28 +1,58 @@
 import * as React from 'react';
 import './App.css';
+import TitleSearchBox from './components/TitleSearchBox';
 
 const api = process.env.REACT_APP_OMDB_API_KEY;
 
 const App = () => {
+  const [searchTitle, setSearchTitle] = React.useState("");
 
+  const handleSearchTitleInput = (e) => {
+    setSearchTitle(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('submit')
+  }
 
   return (
-    <div className='min-h-screen bg-black/80 text-white/90'>
-      <header className='px-4 py-2 bg-yellow-500 text-black'>
-        <h1 className='font-bold text-2xl'>
-          IMDb Search
-        </h1>
-        <p className='text-sm uppercase tracking-wider'>
-          Powered by&nbsp;
-          <a href="http://www.omdbapi.com/" target='_blank'>OMDb</a>
-        </p>
+    <div className='min-h-screen bg-black/90 text-white/90'>
+      <header className='bg-yellow-500 text-black'>
+        <div className='mx-auto px-4 py-2 w-full max-w-screen-lg'>
+          <h1 className='font-bold text-2xl'>
+            IMDb Search
+          </h1>
+          <p className='text-sm uppercase tracking-wider'>
+            Powered by&nbsp;
+            <a href="http://www.omdbapi.com/" target='_blank'>OMDb</a>
+          </p>
+        </div>
       </header>
 
-      <main>
+      <main className='grid content-start'>
+        <section className='bg-black/80'>
+          <div className='mx-auto px-4 py-3 w-full max-w-screen-lg'>
+            <TitleSearchBox 
+              searchTitle={searchTitle}
+              onSearchTitleInput={handleSearchTitleInput}
+              onSubmit={handleSubmit}
+            />
+          </div>
+        </section>
 
+        {searchTitle && (
+          <section className='mx-auto px-4 py-2 w-full max-w-screen-lg'>
+            <p className='text-lg'>
+              Search results for "{searchTitle}"
+            </p>
+          </section>
+        )}
       </main>
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/components/Icons/SearchIcon.js
@@ -0,0 +1,17 @@
+const SearchIcon = ({ className }) => (
+  <svg
+    className={className}
+    fill="none"
+    stroke="currentColor"
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+      strokeLinecap="round"
+      strokeWidth="2"
+    />
+  </svg>
+)
+ 
+export default SearchIcon;
