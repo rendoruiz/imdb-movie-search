ada1dd623a57cded47b85126b816a63e04900e91
Rendo Ruiz <46543419+rendoruiz@users.noreply.github.com>
2022-04-27 21:01:36 -0600

    header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
-function App() {
+const api = process.env.REACT_APP_OMDB_API_KEY;
+
+const App = () => {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className='min-h-screen bg-black/80 text-white/90'>
+      <header className='px-4 py-2 bg-yellow-500 text-black'>
+        <h1 className='font-bold text-2xl'>
+          IMDb Search
+        </h1>
+        <p className='text-sm uppercase tracking-wider'>
+          Powered by&nbsp;
+          <a href="http://www.omdbapi.com/" target='_blank'>OMDb</a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <main>
+
+      </main>
     </div>
   );
 }
